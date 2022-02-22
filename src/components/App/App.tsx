524e4ed8fe741db54a0b6dae7e3fb97294179cb4
Leonardo Fontes <leonardo.prado@g.globo>
2022-02-22 17:56:28 -0300

    first commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from '../../store';
