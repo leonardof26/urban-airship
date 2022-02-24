b76359a6229aed600e4bf3295f0a502b6d8343df
Leonardo Fontes <leonardo.prado@g.globo>
2022-02-24 14:19:55 -0300

    chore: tested

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable eslint-comments/disable-enable-pair */
 /* eslint-disable eslint-comments/no-unlimited-disable */
 /* eslint-disable */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from '../../store';
@@ -9,25 +9,32 @@ import { Layout } from '../Layout/Layout';
 import { Routes } from '../Routes/Routes';
 
 export const App = (): JSX.Element => {
+  const [uaClient, setUaClient] = useState<any>()
+
   async function teste(){
     const ai = await (window as any).UA
+    setUaClient(ai)
     console.log(ai)
   }
 
-  setTimeout(() => {
-    console.log('ihaaa')
-    console.log((window as any).UA);
-    teste()
-  }, 3000)
+  // setTimeout(() => {
+  //   console.log('ihaaa')
+  //   console.log((window as any).UA);
+  //   teste()
+  // }, 3000)
 
   useEffect(() => {
-    console.log('ihe')
+    // console.log('ihe')
 
     if (!(window as any).UA) return;
-    console.log('iha')
-    console.log((window as any).UA);
+    // console.log('iha')
+    // console.log((window as any).UA);
     teste()
   }, [(window as any)?.UA]);
+
+  useEffect(() => {
+    console.log('uaClient', uaClient)
+  }, [uaClient])
 
   return (
     <Provider store={store}>
