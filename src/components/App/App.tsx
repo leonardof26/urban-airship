c1ffbae9b7b71ce44ca44556ae3135a85bb5c3e0
Leonardo Fontes <leonardo.prado@g.globo>
2022-02-24 11:46:38 -0300

    chore: fixed lint problems

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable eslint-comments/no-unlimited-disable */
+/* eslint-disable */
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from '../../store';
 import { Layout } from '../Layout/Layout';
 import { Routes } from '../Routes/Routes';
 
-export const App = (): JSX.Element => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Layout>
-        <Routes />
-      </Layout>
-    </BrowserRouter>
-  </Provider>
-);
+export const App = (): JSX.Element => {
+  async function teste(){
+    const ai = await (window as any).UA()
+    console.log(ai)
+  }
+
+  useEffect(() => {
+    if (!(window as any).UA) return;
+    console.log((window as any).UA);
+    teste()
+  }, [window]);
+
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Layout>
+          <Routes />
+        </Layout>
+      </BrowserRouter>
+    </Provider>
+  );
+};
